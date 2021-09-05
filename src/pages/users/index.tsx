bb03bb52510999bae14d1f987f11ed3ad2ed3c11
Rennan <rennanmax80@gmail.com>
2021-09-05 12:38:46 -0300

    Lista usuários miragejs

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,16 +1,42 @@
-import { Box, Flex, Heading, Button, Text, Icon, Table, Tbody, Td, Thead, Tr, Th, Checkbox, useBreakpointValue } from "@chakra-ui/react";
+import { Box, Flex, Heading, Button, Text, Icon, Table, Tbody, Td, Thead, Tr, Th, Checkbox, useBreakpointValue, Spinner } from "@chakra-ui/react";
 import Link from "next/link";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import {useQuery} from 'react-query'
+
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList(){
+  const {data, isLoading, error} = useQuery('users', async () => {
+    const response = await fetch('http://localhost:3000/api/users')
+    const data = await response.json()
+ 
+    const users = data.users.map(user => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric'
+        }),
+      }
+    });
+
+    return users;
+  }, {
+    staleTime: 100 * 5,
+  })
+
+
 
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   })
+
 
   return(
     <Box>
@@ -36,6 +62,16 @@ export default function UserList(){
             </Link>
           </Flex>
 
+          {isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify="center">
+                <Text>Falha ao obter dados</Text>
+            </Flex>
+          ) : (
+            <>
           <Table colorScheme="whiteAlpha">
             <Thead>
               <Tr>
@@ -49,32 +85,28 @@ export default function UserList(){
               </Tr>
             </Thead>
             <Tbody>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Rennan Maxwell</Text>
-                    <Text fontSize="small" color="gray.300">rennanmax80@gmail.com</Text>
-                  </Box>
-                </Td>
-                { isWideVersion &&  <Td>04 de abril, 2021</Td>}
-                <Td>
-                  <Button 
-                    as="a" 
-                    size="sm" 
-                    fontSize="sm" 
-                    colorScheme="purple"
-                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                  >
-              Editar
-            </Button></Td>
-              </Tr>
+              {data.map(user => {
+                return(
+                  <Tr key={user.id}>
+                  <Td px={["4", "4", "6"]}>
+                    <Checkbox colorScheme="pink" />
+                  </Td>
+                  <Td>
+                    <Box>
+                      <Text fontWeight="bold">{user.name}</Text>
+                      <Text fontSize="small" color="gray.300">{user.email}</Text>
+                    </Box>
+                  </Td>
+                  { isWideVersion &&  <Td>{user.createdAt}</Td>}
+                </Tr> 
+                )  
+              })}
             </Tbody>
-          </Table>
+        </Table>
 
           <Pagination />
+          </>
+          )}
         </Box>
       </Flex>
     </Box>
